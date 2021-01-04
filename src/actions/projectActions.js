8fd284cf6c06de4fed62de2b4fc0fd4a34141c68
Ian Luchini <iancarloluchini@gmail.com>
2021-01-04 18:29:18 -0500

    connected fund button to api post request

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -9,3 +9,18 @@ export const fetchProject = projectId => dispatch => {
         .then(res => dispatch({type: PROJECT_FETCH_SUCCESS, payload: res.data}))
         .catch(err => dispatch({type: PROJECT_FETCH_FAIL, payload: err.response.data}));
 } 
+
+export const fundProject = projectId => dispatch => {
+    // dispatch({type: PROJECT_FETCH_START})
+    (async () => {
+        try {
+            const userRes = await axiosWithAuth().get(`/users/${localStorage.getItem('username')}`);
+            const userId = userRes.data.id;
+            const projectRes = await axiosWithAuth().post(`/projects/${projectId}/fund/${userId}`);
+            dispatch({type: PROJECT_FETCH_SUCCESS, payload: projectRes.data})
+            console.log(projectRes.data);
+        } catch (err) {
+            dispatch({type: PROJECT_FETCH_FAIL, payload: err.response.data.message})
+        }
+    })()
+}
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 
-import { fetchProject } from '../actions/projectActions';
+import { fetchProject, fundProject } from '../actions/projectActions';
+import { Button } from '../components/styles/ButtonStyles';
 
 class Project extends React.Component {
 
@@ -15,13 +16,29 @@ class Project extends React.Component {
         this.props.fetchProject(this.id);
     }
 
+    handleFund = e => {
+        // Any payment system would have to happen somewhere here. 
+        this.props.fundProject(this.id);
+    }
+
     render() {
         return (
             <div>
                 {this.props.message && <p>{this.props.message}</p>}
                 <h2>{this.props.title}</h2>
                 <p>{this.props.description}</p>
-                
+                <div>
+                    <ul>
+                        {this.props.funders.map(funder => 
+                            <li>
+                                <br/>
+                                <p>{funder.firstname} {funder.lastname}</p>
+                                <p>{funder.username}</p>
+                            </li>
+                        )}
+                    </ul>
+                    <Button onClick={this.handleFund}>Fund This Project</Button>
+                </div>
             </div>
         )
     }
@@ -32,4 +49,4 @@ const mapStateToProps = state => {
     return state.project;
 }
 
-export default connect(mapStateToProps, { fetchProject })(withRouter(Project))
+export default connect(mapStateToProps, { fetchProject, fundProject })(withRouter(Project))
